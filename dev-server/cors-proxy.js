763c79ebb295697a2586202096d424a6ec74df13
Benjamin Gruenbaum <benjamingr@gmail.com>
2020-08-30 11:40:44 +0300

    unused package

--- dev-server/cors-proxy.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = require('cors-anywhere').createServer({
-    originWhitelist: ['localhost'], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-});
